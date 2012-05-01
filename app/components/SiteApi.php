d224bc2eefdceede580c5cd15c1ab27707bd1396
Shadez <d.master.me@gmail.com>
2012-05-01 14:32:27 +0900

    Add API server support

---
+++ app/components/SiteApi.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * Copyright (C) 2011-2012 Shadez <https://github.com/Shadez/Framework>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ **/
+
+class SiteApi_Component extends Component
+{
+	public function initialize()
+	{
+		$this->c('Events')
+			->createEvent('onApiSignatureCheck', array($this, 'onApiSignatureCheck'));
+
+		return $this;
+	}
+
+	public function onApiSignatureCheck($event)
+	{
+		return isset($event['eventData']['sig']) ? array('sig' => $event['eventData']) : array();
+	}
+
+	public function isApiMethodImplemented($method)
+	{
+		return method_exists($this, 'apiMethod_' . $method);
+	}
+
+	public function runApiMethod($method, $data)
+	{
+		if (!$method || !isset($method['name']))
+			return false;
+
+		return call_user_func_array(array($this, 'apiMethod_' . $method['name']), array(
+			'method' => $method,
+			'data' => $data
+		));
+	}
+
+	protected function apiMethod_sitegetusername($method, $data)
+	{
+		return array(
+			'username' => 'Shadez'
+		);
+	}
+};
---
+++ framework/components/controller/Api.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Copyright (C) 2011-2012 Shadez <https://github.com/Shadez/Framework>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ **/
+
+class Api_Controller_Component extends Controller_Component
+{
+	protected function run()
+	{
+		$this->ajaxPage();
+
+		// Init site API
+		$this->c('SiteApi');
+
+		$resp = array('errno' => -1, 'errmsg' => 'No method provided');
+
+		if (!isset($_GET['apiSig']))
+		{
+			$resp = array(
+				'errno' => -2,
+				'ermsg' => 'No API Signature provided'
+			);
+		}
+		elseif (isset($_GET['method']))
+			$resp = $this->c('Api')->runApiMethod(addslashes($_GET['method']));
+
+		dump($resp);
+		$this->getCore()->setVar('content', $resp);
+	}
+}
